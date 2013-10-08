a7e7917a984bb6af0fa513a01fc3434228f0b959
Alexis Matherlee <alexismatherlee@gmail.com>
2013-10-07 23:57:43 -0700

    set timer, added reset, added counter

--- Tap Me/ViewController.m
+++ Tap Me/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setupGame];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,7 +27,47 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)buttonPressed {
-    scoreLabel.text = @"Pressed!";
+    count++;
+    scoreLabel.text = [NSString stringWithFormat:@"Score: %i", count];
+    NSLog(@"Pressed!");
+}
+- (void)setupGame {
+    // 1
+    seconds = 30;
+    count = 0;
+    
+    // 2
+    timerLabel.text = [NSString stringWithFormat:@"Time: %i", seconds];
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%i", count];
+    
+    // 3
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(subtractTime)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+- (void)subtractTime {
+    // 1
+    seconds--;
+    timerLabel.text = [NSString stringWithFormat:@"Time: %i",seconds];
+    
+    // 2
+    if (seconds == 0) {
+        [timer invalidate];
+        
+        //2B
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Time is up!"
+                                                        message:[NSString stringWithFormat:@"You scored %i points", count]
+                                                       delegate:self
+                                              cancelButtonTitle:@"Play Again"
+                                              otherButtonTitles:nil];
+        
+        [alert show];
+    }
+}
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self setupGame];
 }
 
 @end
